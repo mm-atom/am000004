1af3b489d672882b97e491c3aff91c62d84f06f3
taoqf <tao_qiufeng@126.com>
2020-03-19 16:32:58 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import am00 from '@mmstudio/am000001';
 
-export default function am4(param1: string) {
+/**
+ * 移除某个key
+ */
+export default function remove(mm: am00, key: string) {
+	const global = mm.global;
+	delete global[key];
 }

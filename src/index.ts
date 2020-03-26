43001cb4dce1eae998e67e564cbab25f87a600ea
taoqf <tao_qiufeng@126.com>
2020-03-26 16:53:06 +0800

    v1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import am00 from '@mmstudio/am000001';
-
 /**
  * 移除某个key
  */
-export default function remove(mm: am00, key: string) {
-	const global = mm.global;
+export default function remove(key: string) {
 	delete global[key];
 }
